191fa14815dcbdf2debc6caf3517d37465b5b9f6
zixiao <2568624492@qq.com>
2022-03-03 23:07:35 +0800

    引入主题

--- src/components/test.vue
+++ src/components/test.vue
@@ -17,21 +17,21 @@ export default {
     },
     methods:{
         intChart(){
-            var myChart =  this.$echarts.init(document.querySelector('.lzj-chart'));
+            var myChart =  this.$echarts.init(document.querySelector('.lzj-chart'),'dark');
             myChart.setOption({
                 title: {
                     text: 'ECharts 入门示例'
                 },
                 tooltip: {},
                 xAxis: {
-                    data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+                    data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子','帽子']
                 },
                 yAxis: {},
                 series: [
                     {
                     name: '销量',
                     type: 'bar',
-                    data: [5, 20, 36, 10, 10, 20]
+                    data: [5, 20, 36, 10, 10, 20,1]
                     }
                 ]
             })
